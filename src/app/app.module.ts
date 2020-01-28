2c0569d9b4a07ded3ddc6bf9179638229c150f8c
Prafful Daga <prraful@gmail.com>
2020-01-28 18:33:38 +0530

    angular components and parent-child relationship

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
-export class AppModule { }
+import { AppRoutingModule } from './app-routing.module';
+import { RootComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { HeaderComponent } from './home/header/header.component';
+import { SidebarComponent } from './home/sidebar/sidebar.component';
+import { NotificationComponent } from './events/notification/notification.component';
+
+//@ -> decorator
+@NgModule(
+  {
+    declarations: [
+                    RootComponent,
+                    HomepageComponent,
+                    HeaderComponent,
+                    SidebarComponent,
+                    NotificationComponent
+                  ],
+    imports: [
+                BrowserModule,
+                AppRoutingModule
+                
+              ],
+    providers: [],
+    bootstrap: [RootComponent]
+
+  })
+export class RootModule { }
