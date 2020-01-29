35c93d621ded165a003aae60f59f33710ed3e20a
Prafful Daga <prraful@gmail.com>
2020-01-29 18:09:20 +0530

    ngSwitchCase, assignment

--- src/app/directive/super/super.component.ts
+++ src/app/directive/super/super.component.ts
@@ -8,13 +8,42 @@ import { Component, OnInit } from '@angular/core';
 export class SuperComponent implements OnInit {
 
   friends = ['ola', 'uber', 'swiggy', 'zoom', 'sophi', 'mary', 'eval']
+  locations = [
+    {
+      city:'chennai',
+      code:'044'
+    },
+    {
+      city:'delhi',
+      code:'011'
+    },
+    {
+      city:'mumbai',
+      code:'022'
+    },
+    {
+      city:'bengaluru',
+      code:'080'
+    }
+  ]
+  display = true
+  displayElse = true
+  name = "prafful"
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  
+  toggleDisplay = function(){
+    this.display = this.display ? false:true
+    console.log(this.display);
+  }
+
+  toggleElseDisplay = function(){
+    this.displayElse = this.displayElse ? false:true
+    console.log(this.displayElse);
+  }
 
   callMe = function(counterReceived){
     console.log("Button Clicked! for " + counterReceived +  ": "  + this.friends[counterReceived]);
